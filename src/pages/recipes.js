ca5169d9946bcf4da9744e4e008159a0712f0bea
Jimmy <jajalaba@yahoo.com.tw>
2022-09-22 19:54:48 +0800

    change recipes

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -3,16 +3,42 @@ import Layout from "../components/Layout"
 import AllRecipes from "../components/AllRecipes"
 import SEO from "../components/SEO"
 import coverImage from "../assets/images/taipei-city2.jpg"
+import RecipesList from "../components/RecipesList"
 
-const recipes = () => {
+import { graphql, useStaticQuery } from "gatsby"
+
+const recipes = ({
+  data: {
+    allContentfulRecipe: { nodes: recipes },
+  },
+}) => {
   return (
     <Layout>
       <SEO title="南港山景點" image={coverImage} />
       <main className="page">
-        <AllRecipes />
+        <RecipesList recipes={recipes} />
       </main>
     </Layout>
   )
 }
+export const query = graphql`
+  query {
+    allContentfulRecipe(sort: { order: ASC, fields: title }) {
+      nodes {
+        view
+        id
+        altitude
+        title
+        slug
+        content {
+          tags
+        }
+        images {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+        }
+      }
+    }
+  }
+`
 
 export default recipes
