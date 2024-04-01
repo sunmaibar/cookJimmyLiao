d72eaf105b913c70e225f3a20a12e51e46574735
Jimmy <jajalaba@yahoo.com.tw>
2024-04-01 14:23:47 +0800

    jn

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="page-footer">
       <p>
-        &copy; {new Date().getFullYear()} <span>南港山系</span> 由{" "}
+        &copy; {new Date().getFullYear()} <span>四獸山全攻略網站</span> 由{" "}
         <a href="https://www.facebook.com/jajalaba/">Jimmy Liao </a>
         建立
       </p>
